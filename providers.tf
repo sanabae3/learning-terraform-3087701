abf811e4feb285d57c2f99748561d10a53601163
Serge <53587568+sanabae3@users.noreply.github.com>
2022-12-16 12:09:31 -0500

    initial state

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
     }
   }
 }
+
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-west-2"
 }
