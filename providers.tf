b18f7f2a7f7edbafa37a607b0f98f54789f2a596
Serge <53587568+sanabae3@users.noreply.github.com>
2022-12-16 13:44:46 -0500

    revert to latter

--- providers.tf
+++ providers.tf
@@ -1,14 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   version = "~> 4.0"
-  region = "us-east-1"
+  region  = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
